536528c41b968ab5579ec20fa4041da80e3ae4c4
ColinGreybosh <cgreybosh99@gmail.com>
2017-09-09 22:15:35 -0400

    Extended .gitignore, added .gitattributes

--- Project Euler - Problem 3/Project Euler - Problem 3/main.cpp
+++ Project Euler - Problem 3/Project Euler - Problem 3/main.cpp
@@ -3,3 +3,59 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
 */
+
+#include <iostream>
+#include <cmath>
+#include "main.h"
+
+bool isPrime(long long numberToCheck)
+{
+    bool isPrime = true;
+
+    if (numberToCheck < 0)
+    {
+        return false;
+    }
+    else if (numberToCheck == 0 || numberToCheck == 1)
+    {
+        return false;
+    }
+    else if (numberToCheck == 2)
+    {
+        return true;
+    }
+    else if (numberToCheck % 2 == 0)
+    {
+        return false;
+    }
+    else
+    {
+        for (long long i = 3; i <= static_cast<long long>(sqrt(numberToCheck)); i += 2)
+        {
+            if (numberToCheck % i == 0)
+            {
+                isPrime = false;
+            }
+        }
+    }
+
+    return isPrime;
+}
+
+
+int main(int argc, char* args[])
+{
+    const long long int divisor = 600851475143;
+    long long largestPrimeFactor = 0;
+
+    if (ceil(sqrtl(600851475143)) % 2 == 0)
+    {
+
+    }
+
+    std::cout << largestPrimeFactor;
+
+    getchar();
+
+    return 0;
+}
